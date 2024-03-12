35197757a69052be51ce941958e73065a56a3e0d
Lilly Zwikker <reinier@zwikker.eu>
2024-03-12 17:42:39 +0100

    Bug fixes

--- sw/airborne/modules/corr_depth_finder/corr_depth_finder.h
+++ sw/airborne/modules/corr_depth_finder/corr_depth_finder.h
@@ -5,4 +5,7 @@
 #ifndef PAPARAZZI_GROUP_4_CORR_DEPTH_FINDER_H
 #define PAPARAZZI_GROUP_4_CORR_DEPTH_FINDER_H
 
+extern void corr_depth_finder_init(void);
+extern void corr_depth_finder_periodic(void);
+
 #endif //PAPARAZZI_GROUP_4_CORR_DEPTH_FINDER_H

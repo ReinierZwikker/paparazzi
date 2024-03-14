6ef8d7b0c043adabe4422bb50ac17bd8d3240055
Mauro <maubeend@gmail.com>
2024-03-14 17:37:46 +0100

    Added some definitions to the header of video capture

--- sw/airborne/modules/computer_vision/video_capture.h
+++ sw/airborne/modules/computer_vision/video_capture.h
@@ -26,6 +26,7 @@
 #ifndef VIDEO_CAPTURE_H_
 #define VIDEO_CAPTURE_H_
 
+#include "modules/computer_vision/lib/vision/image.h"
 #include <stdbool.h>
 
 // Module settings
@@ -33,28 +34,7 @@ extern bool video_capture_take_shot;
 extern bool video_capture_record_video;
 
 // Module structures
-/* The different type of images we currently support */
-enum image_type {
-    IMAGE_YUV422,     ///< UYVY format (uint16 per pixel)
-    IMAGE_GRAYSCALE,  ///< Grayscale image with only the Y part (uint8 per pixel)
-    IMAGE_JPEG,       ///< An JPEG encoded image (not per pixel encoded)
-    IMAGE_GRADIENT,    ///< An image gradient (int16 per pixel)
-    IMAGE_INT16     ///< An image to hold disparity image data from openCV (int16 per pixel)
-};
-
-/* Main image structure */
-struct image_t {
-    enum image_type type;   ///< The image type
-    uint16_t w;             ///< Image width
-    uint16_t h;             ///< Image height
-    struct timeval ts;      ///< The timestamp of creation
-    struct FloatEulers eulers;   ///< Euler Angles at time of image
-    uint32_t pprz_ts;       ///< The timestamp in us since system startup
-
-    uint8_t buf_idx;        ///< Buffer index for V4L2 freeing
-    uint32_t buf_size;      ///< The buffer size
-    void *buf;              ///< Image buffer (depending on the image_type)
-};
+struct image_t;
 
 // Module functions
 extern void video_capture_init(void);
